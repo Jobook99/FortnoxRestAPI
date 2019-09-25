65db1e9a01bd973e9cf85a45520e08416db4adca
Jobook <43270329+Jobook99@users.noreply.github.com>
2019-09-25 17:32:33 +0200

    Update

--- src/java/web/service/BoxesFacadeREST.java
+++ src/java/web/service/BoxesFacadeREST.java
@@ -37,45 +37,11 @@ public class BoxesFacadeREST extends AbstractFacade<Boxes> {
         super.create(entity);
     }
 
-    @PUT
-    @Path("{id}")
-    @Consumes({"application/xml", "application/json"})
-    public void edit(@PathParam("id") Integer id, Boxes entity) {
-        super.edit(entity);
-    }
-
-    @DELETE
-    @Path("{id}")
-    public void remove(@PathParam("id") Integer id) {
-        super.remove(super.find(id));
-    }
-
-    @GET
-    @Path("{id}")
-    @Produces({"application/xml", "application/json"})
-    public Boxes find(@PathParam("id") Integer id) {
-        return super.find(id);
-    }
-
     @GET
     @Override
     @Produces({"application/json"})
     public List<Boxes> findAll() {
         return super.findAll();
-    }
-
-    @GET
-    @Path("{from}/{to}")
-    @Produces({"application/xml", "application/json"})
-    public List<Boxes> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
-        return super.findRange(new int[]{from, to});
-    }
-
-    @GET
-    @Path("count")
-    @Produces("text/plain")
-    public String countREST() {
-        return String.valueOf(super.count());
     }
 
     @Override
