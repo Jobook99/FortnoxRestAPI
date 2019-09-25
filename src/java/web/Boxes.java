b62ad4d9caebc00118f44d819e038d157b221249
Jobook <43270329+Jobook99@users.noreply.github.com>
2019-09-25 18:45:03 +0200

    Update3

--- src/java/web/Boxes.java
+++ src/java/web/Boxes.java
@@ -117,6 +117,26 @@ public class Boxes implements Serializable {
     public void setGreen(int green) {
         this.green = green;
     }
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Boxes)) {
+            return false;
+        }
+        Boxes other = (Boxes) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
 
     @Override
     public String toString() {
